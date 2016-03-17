4b30c5e2a871b0e2d756a5fdd32ff6f5e85f9f02
Darrel <darrel@tavis.ca>
2016-03-17 10:33:00 -0400

    Enabled Swagger UI for build demo

--- ConferenceApiApp/App_Start/SwaggerConfig.cs
+++ ConferenceApiApp/App_Start/SwaggerConfig.cs
@@ -172,11 +172,11 @@ namespace ConferenceApiApp
                         //
                         c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
                         // ***** Uncomment the following to enable the swagger UI *****
-                        /*
+                        
                             })
                         .EnableSwaggerUi(c =>
                             {
-                        */
+                        
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
                         // "Logical Name" is passed to the method as shown below.
