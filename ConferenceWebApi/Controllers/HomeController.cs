3c9f450db470b6e9a668b9bd5ba3b2f696df1efc
Darrel Miller <darrmi@microsoft.com>
2017-08-15 16:08:08 -0400

    Added support for JSON rendering of OpenAPI

--- ConferenceWebApi/Controllers/HomeController.cs
+++ ConferenceWebApi/Controllers/HomeController.cs
@@ -4,7 +4,10 @@ using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Results;
 using ConferenceWebApi.Tools;
-
+using SharpYaml.Serialization;
+using System.IO;
+using System.Text;
+using SharpYaml;
 
 namespace ConferenceWebApi.Controllers
 {
@@ -13,12 +16,30 @@ namespace ConferenceWebApi.Controllers
     public class HomeController : ApiController
     {
         
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(string format = "yaml")
         {
-            var content = new StreamContent(this.GetType().Assembly.GetManifestResourceStream("ConferenceWebApi.OpenApi2.yaml"));
-            content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+            var stream = this.GetType().Assembly.GetManifestResourceStream("ConferenceWebApi.OpenApi2.yaml");
+            HttpContent responseContent;
+
+            if (format=="json" || this.Request.Headers.Accept.Contains(new MediaTypeWithQualityHeaderValue("application/json")))
+            {
+                var serializer = new Serializer(new SerializerSettings() { EmitJsonComptible = true });
+                var yamlObject = serializer.Deserialize(stream);
+
+                var buffer = new StringBuilder();
+                var jserializer = new Newtonsoft.Json.JsonSerializer();
+                jserializer.Serialize(new StringWriter(buffer), yamlObject);
+                responseContent = new StringContent(buffer.ToString());
+                responseContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            } else
+            {
+                responseContent = new StreamContent(stream);
+                responseContent.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+            }
+
+            
             return new OkResult(Request)
-                .WithContent(content);
+                .WithContent(responseContent);
         }
     }
 
